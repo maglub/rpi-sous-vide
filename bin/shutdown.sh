03ae550393f13bfc3bbd3cb98ac58b62485a017a
Magnus Lubeck <magnus.lubeck@gmail.com>
2017-07-10 08:17:52 +0000

    minor changes

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -2,12 +2,14 @@
 
 this_dir=$(cd $(dirname $0); pwd)
 . $this_dir/../conf/app.conf
+. $this_dir/functions
 
 #--- Zero the control files, to avoid accidents
 echo 0 > $temperatureFile
 echo 0 > $heaterDutyFile
 echo 0 > $setpointFile 
 
-/home/pi/rpi-sous-vide/bin/wrapper killProcesses
-/home/pi/rpi-sous-vide/bin/wrapper setHeater off
+#--- call functions in the functions file
+killProcesses
+setHeater off
 
