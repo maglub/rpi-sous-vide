c15d3696fc1867682e1fc4def2807024840c3be5
Magnus Lubeck <magnus.lubeck@gmail.com>
2017-10-25 16:44:50 +0000

    Beautified the script a bit

--- setup-influxdb.sh
+++ setup-influxdb.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get -y install apt-transport-https
+this_dir=$(cd $(dirname $0);pwd)
 
-curl -sL https://repos.influxdata.com/influxdb.key  | sudo apt-key add -
 . /etc/os-release 
 
-test $VERSION_ID = "7" && echo "deb https://repos.influxdata.com/debian wheezy stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-test $VERSION_ID = "8" && echo "deb https://repos.influxdata.com/debian jessie stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-test $VERSION_ID = "9" && echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+echo -n "* Checking pre requisite apt-transport-https"
+dpkg -s apt-transport-https >/dev/null 2>&1 || {
+  echo "  - Installing apt-transport-https"
+  sudo apt-get update
+  sudo apt-get -y install apt-transport-https
+}
+echo "  - Done!"
 
-sudo apt-get update && sudo apt-get -y install influxdb
+echo -n "* Installing repo key for influxdb: "
+curl -sL https://repos.influxdata.com/influxdb.key  | sudo apt-key add -
+echo "  - Done!"
 
+
+echo -n "* Checking for influxdb repo source"
+[ ! -f /etc/apt/sources.list.d/influxdb.list ] && {
+  echo "  - Adding repo for influxdb"
+
+  test $VERSION_ID = "7" && echo "deb https://repos.influxdata.com/debian wheezy stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+  test $VERSION_ID = "8" && echo "deb https://repos.influxdata.com/debian jessie stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+  test $VERSION_ID = "9" && echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+}
+echo "  - Done!"
+
+echo -n "* Checking if influxdb is installed"
+dpkg -s influxdb >/dev/null 2>&1 || {
+  echo "  - Installing influxdb"
+  sudo apt-get update && sudo apt-get -y install influxdb
+}
+echo "  - Done!"
+
+echo -n "* Enabling influxdb at boot"
 case $VERSION_ID in
   7)
     sudo update-rc.d influxdb defaults
@@ -22,10 +45,20 @@ case $VERSION_ID in
     sudo systemctl start influxdb.service
     ;;
 esac
+echo "  - Done!"
 
 #--- create the database
+echo -n "* Checking for database: smoker"
+influx -execute "show databases;" | grep -w smoker --silent || {
+echo "  - Creating database: smoker"
 influx -execute "create database smoker;"
+}
+echo "  - Done!"
 
 #--- enable logging of metrics to the influxdb
-ln -s ../logging-available/influxdb.local bin/logging-enabled/influxdb.local
+echo -n "* Enabling influxdb logging for the application"
+[ ! -h $this_dir/bin/logging-enabled/influxdb.local ] && {
+  ln -s ../logging-available/influxdb.local $this_dir/bin/logging-enabled/influxdb.local
+}
+echo "  - Done!"
 
